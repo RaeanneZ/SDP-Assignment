384bde5a2b69badf30beba9edb5e57a12073c173
SianKim <siankim06@gmail.com>
2025-01-31 10:34:49 +0800

    Finished Strategy

--- Program.cs
+++ Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Create a document
+            User owner = new User("Anna");
+            Document document = new Document("Technical Report", "This is the content of the document.", owner);
+
+            // Set format converter to Word
+            document.SetFormatConverter(new WordConverter());
+            document.ConvertDocument();
+
+            // Change format converter to PDF
+            document.SetFormatConverter(new PDFConverter());
+            document.ConvertDocument();
         }
     }
 }
---
+++ WordConverter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SDP_Assignment
+{
+    public class WordConverter : FormatConverter
+    {
+        public void Convert(string content)
+        {
+            Console.WriteLine("Converting document to Word format...");
+            // Actual conversion logic to Word format
+            Console.WriteLine($"Word Content: {content}");
+        }
+    }
+}
